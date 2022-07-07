cadc1b8bf2b3f21895e032bc128bfef1a65587bc
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-07 15:35:31 +0900

    Create [2440번] 별 찍기 - 3.cpp
    BaekJoon [2440번] 별 찍기 - 3

---
+++ [2440번] 별 찍기 - 3.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    deque<char> dq;
+    int num;
+    cin >> num;
+    for (int i = 0; i < num; ++i) {
+        dq.push_back('*');
+    }
+
+    for (int i = 0; i < num; ++i) {
+        int len = dq.size();
+        for (int j = 0; j < len; ++j) {
+            cout << dq[j];
+        }
+        cout << '\n';
+        dq.pop_back();
+    }
+
+    return 0;
+}

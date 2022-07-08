a2dcf80fb1b818005b1ce48323138d00e78c8eb6
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-08 14:00:57 +0900

    Create [2444번] 별 찍기 - 7.cpp
    BaekJoon [2444번] 별 찍기 - 7

---
+++ [2444번] 별 찍기 - 7.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
+
+    int num;
+    cin >> num;
+    
+    for(int i = 1; i <= num; ++i) {
+        // 규칙 2 * n - 1
+        int starvalue = 2 * i -1 ;
+
+        for(int j = i; j < num; ++j) {
+            cout <<' ';
+        }
+
+        for(int k = 1; k <= starvalue; ++k) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    for(int i = num -1; i >= 1; --i) {
+        // 규칙 2 * n - 1
+        int starvalue = 2 * i -1 ;
+
+        for(int j = i; j < num; ++j) {
+            cout <<' ';
+        }
+
+        for(int k = 1; k <= starvalue; ++k) {
+            cout << '*';
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}

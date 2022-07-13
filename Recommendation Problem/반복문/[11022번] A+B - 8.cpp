20563bf1c7a3abdd825dd07b7544cb99c82d040e
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 09:40:45 +0900

    Create [11022번] A+B - 8.cpp
    BaekJoon [11022번] A+B - 8

---
+++ Recommendation Problem/반복문/[11022번] A+B - 8.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; ++i) {
+        int A, B;
+        cin >> A >> B;
+        cout <<"Case #" << i <<": " << A << " + " << B << " = " << A+B << '\n';
+    }
+
+    return 0;
+}

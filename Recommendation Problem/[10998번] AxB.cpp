4ffd8d33b245b50cf03630bb84d95dc8125975da
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 09:06:11 +0900

    Create [10998번] AxB.cpp
    BaekJoon [10998번] AxB

---
+++ Recommendation Problem/[10998번] AxB.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int A,B ;
+    cin >> A >> B;
+    cout << A*B;
+
+    return 0;
+}

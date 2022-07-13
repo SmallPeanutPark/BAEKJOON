860cf3a42de2837dc1b6414a81411926899bfea7
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 09:50:41 +0900

    Create [2741번] N 찍기.cpp
    BaekJoon [2741번] N 찍기

---
+++ Recommendation Problem/반복문/[2741번] N 찍기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; ++i) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}

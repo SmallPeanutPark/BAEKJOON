9e265fdd476d6a0c3d4349d8641ebb9437ba7b6a
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 16:27:26 +0900

    Create [18108번] 1998년생인 내가 태국에서는 2541년생?!.cpp
    BaekJoon [18108번] 1998년생인 내가 태국에서는 2541년생?!

---
+++ Recommendation Problem/입출력과 사칙연산/[18108번] 1998년생인 내가 태국에서는 2541년생?!.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int y;
+    cin >> y; // 불기연도
+    // 차이 -543년
+    cout << y - 543;
+}

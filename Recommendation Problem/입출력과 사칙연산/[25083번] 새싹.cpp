f2919842d49a12ae4118ef5e5629a913b24a1d3b
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 16:39:51 +0900

    Create [25083번] 새싹.cpp
    BaekJoON [25083번] 새싹

---
+++ Recommendation Problem/입출력과 사칙연산/[25083번] 새싹.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cout << "         " << ',' << 'r' << "'" << "\"" <<'7' << '\n';
+    cout << 'r' << '`' << '-' << '_' << "   " << ',' << "'" << "  ,/" << '\n';
+    cout << ' ' << '\\' << '.' << ' ' << '\"' << '.' << ' ' << 'L' << '_' << 'r' << '\''<< '\n';
+    cout << "   " << '`' << '~'<< '\\' << '/' << '\n';
+    cout << "      " << '|' << '\n';
+    cout << "      " << '|' << '\n';
+}

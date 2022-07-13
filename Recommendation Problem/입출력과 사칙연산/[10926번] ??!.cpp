64f0096ef7d6b41dc01fe055fb2026b34e16a8bc
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 16:23:57 +0900

    Create [10926번] ??!.cpp
    BaekJoon [10926번] ??!

---
+++ Recommendation Problem/입출력과 사칙연산/[10926번] ??!.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    string s;
+    cin >> s;
+    s += "??!";
+    cout << s;
+}

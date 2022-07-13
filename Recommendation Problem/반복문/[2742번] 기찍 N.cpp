af84d8803250db569872f9516f4f969162ade88d
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 09:52:03 +0900

    Create [2742번] 기찍 N.cpp
    BaekJoon [2742번] 기찍 N

---
+++ Recommendation Problem/반복문/[2742번] 기찍 N.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    for(int i = N; i >= 1; --i) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}

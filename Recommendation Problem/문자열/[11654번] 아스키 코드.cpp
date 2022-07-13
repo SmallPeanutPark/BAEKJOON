73521ebde7f7d8a1b41d2fa95e83cab740095ed6
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 13:51:30 +0900

    Create [11654번] 아스키 코드.cpp
    BaekJoon [11654번] 아스키 코드

---
+++ Recommendation Problem/문자열/[11654번] 아스키 코드.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    char c;
+    cin >> c;
+    int a = c;
+    cout << a;
+    return 0;
+}

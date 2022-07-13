2f683a221da9f79f7335f7c421b16b88bbcff0d2
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 09:08:53 +0900

    Create [10718번] We love kriii.cpp
    BaekJoon [10718번] We love kriii

---
+++ Recommendation Problem/[10718번] We love kriii.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    cout << "강한친구 대한육군" << '\n';
+    cout << "강한친구 대한육군";
+
+    return 0;
+}

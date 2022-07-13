31b06622c13ff889e858848be292ebc2979d652f
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-07-13 16:31:22 +0900

    Create [10430번] 나머지.cpp
    BaekJoon [10430번] 나머지

---
+++ Recommendation Problem/입출력과 사칙연산/[10430번] 나머지.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A+B) % C << '\n';
+    cout << ((A%C) + (B%C))%C << '\n';
+    cout << (A*B)%C << '\n';
+    cout << ((A%C) * (B%C))%C << '\n';
+}

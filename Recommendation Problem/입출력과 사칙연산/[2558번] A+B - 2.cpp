6cec156bf52740159bddf0ee3009e40bbeb004c6
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-04 14:31:23 +0900

    Create [2558번] A+B - 2.cpp
    BaekJoon [2558번] A + B - 2

---
+++ Recommendation Problem/입출력과 사칙연산/[2558번] A+B - 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int A, B;
+    cin >> A;
+    cin >> B;
+    cout << A + B;
+}

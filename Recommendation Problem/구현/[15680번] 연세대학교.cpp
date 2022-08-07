3e27d9fdea736a782037ccd13a60d737e52ef28d
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-07 21:14:57 +0900

    Create [15680번] 연세대학교.cpp
    BaekJoon [15680번] 연세대학교

---
+++ Recommendation Problem/구현/[15680번] 연세대학교.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+    if(N == 0) 
+        cout << "YONSEI";
+    else
+        cout << "Leading the Way to the Future";
+
+    return 0;
+}

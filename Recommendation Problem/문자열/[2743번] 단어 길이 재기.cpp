a8a31a11d4a91d836f47b3da4a6a1ad1640e4677
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-08 11:15:39 +0900

    Create [2743번] 단어 길이 재기.cpp
    BaekJoon [2743번] 단어 길이 재기

---
+++ Recommendation Problem/문자열/[2743번] 단어 길이 재기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string str;
+    cin >> str;
+    cout << str.length();
+    return 0;
+}

3efbd1965fa76ba295bd0ce0d07ec285e76d8b72
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-09 10:44:29 +0900

    Create [5717번] 상근이의 친구들.cpp
    BaekJoon [5717번] 상근이의 친구들

---
+++ Recommendation Problem/수학/[5717번] 상근이의 친구들.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    int M, F;
+    while(cin >> M >> F) {
+        if((M == 0) && (F == 0)) break;
+        cout << M + F << '\n';
+    }
+    return 0;
+}

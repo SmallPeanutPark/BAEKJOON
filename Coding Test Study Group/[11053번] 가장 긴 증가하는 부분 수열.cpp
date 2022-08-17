5a7d3dd494f4c6a050eae858bdc721efa215b829
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-17 21:38:35 +0900

    Update [11053번] 가장 긴 증가하는 부분 수열.cpp
    이해하기 쉽게 코드 바꿈

--- Coding Test Study Group/[11053번] 가장 긴 증가하는 부분 수열.cpp
+++ Coding Test Study Group/[11053번] 가장 긴 증가하는 부분 수열.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int N; // 수열의 크기
 
 int lengthOfLIS(vector<int>& A) {
-    vector<int> dp(N, 1); // 최소 1의 길이를 가질 수 있으므로 1로 초기화
-    // dp vector는 A[i]를 마지막 값으로 가지는 최장 증가 부분 수열의 길이를 의미
+    vector<int> dp(N, 1);
     for(int i = 0; i < N; ++i) {
         for(int j = 0; j < i; ++j) {
-            // 현재 값이 이전 값보다 커야하고, 
-            // 현재 값을 마지막으로 가지는 가장 긴 증가 수열의 길이가 이전 값으로 가지는 가장 긴 증가 수열의 길이보다 작다면
-            if(A[i] > A[j] && dp[i] < dp[j] + 1) {
-                dp[i] = dp[j] + 1; // 증가
+            if(A[i] > A[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
             }
         }
     }

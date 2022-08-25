930fb07506eee2166a9ceb28fd4aeebf86a7ccfb
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-08-25 10:54:42 +0900

    Create [11382번] 꼬마 정민.cpp
    BaekJoon [11382번] 꼬마 정민

---
+++ Personal Study/수학/[11382번] 꼬마 정민.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = unsigned long long;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ull A, B, C;
+    cin >> A >> B >> C;
+    ull sum = A + B + C;
+    cout << sum;
+    return 0;
+}

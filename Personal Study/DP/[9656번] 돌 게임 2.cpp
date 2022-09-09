654b58175d736860b4dca776c23d47d83368a672
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-09-09 17:59:42 +0900

    Create [9656번] 돌 게임 2.cpp
    BaekJoon [9656번] 돌 게임 2

---
+++ Personal Study/DP/[9656번] 돌 게임 2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N; cin >> N;
+    if(N % 2 == 0) cout << "SK";
+    else cout << "CY";
+    return 0;
+}

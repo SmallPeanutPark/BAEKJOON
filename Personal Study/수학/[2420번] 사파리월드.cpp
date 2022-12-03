557cbd9cc155a430802cbe1da2514882b5f6d017
땅콩 <108335098+SmallPeanutPark@users.noreply.github.com>
2022-12-03 20:15:36 +0900

    Create [2420번] 사파리월드.cpp
    BaekJoon [2420번] 사파리월드

---
+++ Personal Study/수학/[2420번] 사파리월드.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long N, M;
+    cin >> N >> M;
+    cout << llabs(N-M) << '\n';
+    return 0;
+}
